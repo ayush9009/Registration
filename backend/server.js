81df4ee6a4ae4274344379a5e877deac5ba5c056
Ayush Sharma <97290036+ayush9009@users.noreply.github.com>
2024-01-18 18:13:31 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -7,14 +7,14 @@ const User = require('./models/User');
 
 const app = express();
 // app.use(express.json());
-// app.use(cors());
-app.use(cors(
-    {
-        origin:["https://register-steel.vercel.app/"],
-        methods:["POST","GET"],
-        credentials:true
-    }
-));
+app.use(cors());
+// app.use(cors(
+//     {
+//         origin:["https://register-steel.vercel.app/"],
+//         methods:["POST","GET"],
+//         credentials:true
+//     }
+// ));
 app.use(express.json());
 
 
